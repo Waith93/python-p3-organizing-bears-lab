cf48413480a3ae165b0ecbd6c82509293e28941f
Waithera <stacywaithera@DESKTOP-E0D09HN>
2025-05-21 00:34:57 +0300

    organizing bears

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy",4, "M", "Black", "Aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha",5, "F", "Blonde", "Playful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown",6, "F", "Dark brown", "Grumpy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa",1, "F", "White", "Friendly", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch",9 ,"M", "Green", "Moody", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 7, "F", "Grey", "Shy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, "M", "Brown", "Curious", 0);
